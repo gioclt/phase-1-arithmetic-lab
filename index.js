b90a08c97d77758b771ea115205d0299a3c8159c
Yobany Caltempa <gioclt26@gmail.com>
2022-03-01 17:14:13 -0500

    commit tree 6a1e4676a3a0b0799524a12779f8cb8cc37ef6f0

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1  = 2
+let num2 = 31
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = 1 * 4
+
+let num3 = 16
+let num4 = 4
+let mod = 4
+let remainder = num3 % num4
+
+let max = 20
+Math.max(16, 17, 18, 19, 20)
